170b36d750bac216113f7454adc8c34a2f32f2a1
rashidhussain85 <44375381+rashidhussain85@users.noreply.github.com>
2018-10-23 14:32:13 -0700

    hard code connection string

--- conn.php
+++ conn.php
@@ -1,8 +1,5 @@
 <?php
-$dbhost = $_SERVER['RDS_HOSTNAME'];
-$username = $_SERVER['RDS_USERNAME'];
-$password = $_SERVER['RDS_PASSWORD'];
-mysql_connect({$dbhost}, {$username},{$password})or
+mysql_connect('crcko5ndnoq3fh.cpehvzkhq3fu.ap-southeast-2.rds.amazonaws.com', 'dbuser','dbpassword')or
 		die("Could not connect: " . mysql_error());
 		mysql_select_db("db");
 		
