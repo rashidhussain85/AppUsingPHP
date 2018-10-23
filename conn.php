566de350059266e24154d00dba6dd5872bf9bdd4
rashidhussain85 <44375381+rashidhussain85@users.noreply.github.com>
2018-10-23 13:11:27 -0700

    Used variable RDS Endpoint

--- conn.php
+++ conn.php
@@ -1,5 +1,8 @@
 <?php
-mysql_connect('myrdsdb.c0uoizh0juf5.ap-southeast-2.rds.amazonaws.com', 'test','Test1234')or
+$dbhost = $_SERVER['RDS_HOSTNAME'];
+$username = $_SERVER['RDS_USERNAME'];
+$password = $_SERVER['RDS_PASSWORD'];
+mysql_connect({$dbhost}, {$username},{$password})or
 		die("Could not connect: " . mysql_error());
 		mysql_select_db("db");
 		
