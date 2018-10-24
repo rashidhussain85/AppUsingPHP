4b32a43edb7d444c6d83c92614004f4795ea0128
rashidhussain85 <44375381+rashidhussain85@users.noreply.github.com>
2018-10-24 09:57:30 -0700

    update connection string

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect('crcko5ndnoq3fh.cpehvzkhq3fu.ap-southeast-2.rds.amazonaws.com', 'dbuser','dbpassword')or
+mysql_connect('cr12gn8s807t0ar.cpehvzkhq3fu.ap-southeast-2.rds.amazonaws.com', 'dbuser','dbpassword')or
 		die("Could not connect: " . mysql_error());
 		mysql_select_db("db");
 		
